eb0906f461300ebb74be1fac3d57d5ae1a5e1708
headblockhead <26520767+headblockhead@users.noreply.github.com>
2020-06-09 12:09:11 +0100

    First Commit

---
+++ Chatbot.py
@@ -0,0 +1,16 @@
+import random
+name = input("Chatbot > What is your name?\nNameless User 1 > ")
+questions = ["What is your favorite food?", "How old are you?", "Do you like sweets?", "When Were you born?"]
+question = random.choice(questions)
+print("Chatbot > " + question)
+answer = input(name + " > ")
+if question == questions[0,0] :
+        food = answer
+elif question == questions[1,1] and answer == numbers.Number:
+
+    age = answer
+elif question == questions[2,2] and answer == "yes" or answer == "no" :
+    if answer == "yes":
+        sweetslike == True
+    else:
+        sweetslike == False
