683c9fbee2926b073fe6b70bdda607940dad0a4a
headblockhead <26520767+headblockhead@users.noreply.github.com>
2020-06-09 14:36:03 +0100

    Added Answer checking

--- Chatbot.py
+++ Chatbot.py
@@ -1,16 +1,21 @@
 import random
+import numbers
+import warnings
 name = input("Chatbot > What is your name?\nNameless User 1 > ")
-questions = ["What is your favorite food?", "How old are you?", "Do you like sweets?", "When Were you born?"]
+questions = ["What is your favorite food?", "How old are you?", "Do you like sweets?"]
 question = random.choice(questions)
 print("Chatbot > " + question)
 answer = input(name + " > ")
-if question == questions[0,0] :
-        food = answer
-elif question == questions[1,1] and answer == numbers.Number:
-
-    age = answer
-elif question == questions[2,2] and answer == "yes" or answer == "no" :
+if question == questions[0] :
+    food = answer
+elif question == questions[2] and answer == "yes" or answer == "no" :
     if answer == "yes":
-        sweetslike == True
+        sweetslike = True
     else:
-        sweetslike == False
+        sweetslike = False
+else:
+    try:
+        if question == questions[1] and isinstance(int(answer), numbers.Number):
+            age = answer
+    except:
+        print("That answer doesn't make sense!")
